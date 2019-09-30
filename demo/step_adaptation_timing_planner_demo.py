2409fe4835c9dce61fa32c19f83d443fffba040e
avadesh02 <avadesh02@gmail.com>
2019-09-30 17:18:54 -0400

    basic trajectory generator implemented. reactive planner not working properly yet.

--- demo/step_adaptation_timing_planner_demo.py
+++ demo/step_adaptation_timing_planner_demo.py
@@ -31,9 +31,9 @@ l_min = 0.0
 l_max = 0.2
 w_min = 0.0
 w_max = 0.2
-t_min = 0.1
-t_max = 1
-v_des = [0.2,0]
+t_min = 0.01
+t_max = 0.6
+v_des = [0.0,0]
 l_p = 0
 ht = 0.22
 
@@ -56,7 +56,7 @@ robot.reset_state(q0, dq0)
 
 x_des = 4*[0.0, 0.0, -0.25]
 xd_des = 4*[0,0,0] 
-kp = 4 * [200,200,200]
+kp = 4 * [200,200,500]
 kd = 4 * [10.0,10.0,10.0]
 f = np.zeros(18)
 f = 4*[0.0, 0.0, (2.2*9.8)/4]
@@ -66,17 +66,22 @@ solo_leg_ctrl = solo_impedance_controller(robot)
 
 # Run the simulator for 100 steps
 t = 0
+t_gap = 0.05
 t_end = t_max
 n = 1
 u_current_step = [0.0, 0.0]
 x_com = np.array([0.0, 0.0]) 
 xd_com = np.array([0.0, 0.0]) 
 
-for i in range(4000):
+x_bias = [0.2, 0.1, 0.0, 0.2, -0.1, 0.0, -0.2, 0.1, 0.0, -0.2, -0.1, 0.0]
+
+tmp = []
+
+for i in range(6000):
     # TODO: Implement a controller here.    
     # Step the simulator.
     p.stepSimulation()
-    time.sleep(0.001) # You can sleep here if you want to slow down the replay
+    time.sleep(0.0001) # You can sleep here if you want to slow down the replay
     # Read the final state and forces after the stepping.
 
     q, dq = robot.get_state()
@@ -84,16 +89,52 @@ for i in range(4000):
     x_com[1] = float(q[1])
     xd_com[0] = float(dq[0])
     xd_com[1] = float(dq[1])
+
+    ## computing the current location of the legs FL, FR
+    fl_location = np.array(solo_leg_ctrl.FL_imp.compute_distance_between_frames(q))
+    fl_location = np.reshape(fl_location, (3,))
+    fr_location = np.array(solo_leg_ctrl.FR_imp.compute_distance_between_frames(q))
+    fr_location = np.reshape(fr_location, (3,))
     if t < t_end:
-        dcm_t = dcm_vrp_planner.compute_dcm_current(x_com,xd_com)
-        x_opt = dcm_vrp_planner.compute_adapted_step_locations(u_current_step, t, n, dcm_t)
-        t_end = x_opt[2]
+        if t_end - t > t_gap: 
+            ### This if statement prevents adaptation near the end of the step to prevents jumps in desrired location.
+            dcm_t = dcm_vrp_planner.compute_dcm_current(x_com,xd_com)
+            x_opt = dcm_vrp_planner.compute_adapted_step_locations(u_current_step, t, n, dcm_t)
+            t_end = x_opt[2]
+            if np.power(-1, n) > 0:
+                x_des_fl_hr,xd_des_fl_hr = dcm_vrp_planner.generate_foot_trajectory(x_opt[0:2], t_end, t, fl_location, -.1, -0.26)
+                x_des[0:3] = np.reshape(x_des_fl_hr, (3,))
+                x_des[3:5] = u_current_step
+                x_des[6:8] = u_current_step
+                x_des[9:12] = np.reshape(x_des_fl_hr, (3,))
+                x_des += x_bias
+                tmp.append(x_des[2])
+                # xd_des = 4*[0,0,0] 
+                # xd_des[0:3] = xd_des_fl_hr 
+                # xd_des[6:9] = xd_des_fl_hr
+            else:
+                x_des_fr_hl, xd_des_fr_hl = dcm_vrp_planner.generate_foot_trajectory(x_opt[0:2], t_end, t, fr_location, -0.1, -0.26)
+                x_des[0:2] = u_current_step 
+                x_des[3:6] = np.reshape(x_des_fr_hl, (3,))
+                x_des[6:9] = np.reshape(x_des_fr_hl, (3,))
+                x_des[9:11] = u_current_step
+                x_des += x_bias
+                
+                # xd_des = 4*[0,0,0] 
+                # xd_des[3:6] = xd_des_fr_hl
+                # xd_des[6:9] = xd_des_fr_hl
         t+=0.001
-        
+    
     else:
-        print("in else")
         t = 0
         u_current_step = [x_opt[0],x_opt[1]]
+        # print(u_current_step)
         n += 1
     tau = solo_leg_ctrl.return_joint_torques(q,dq,kp,kd,x_des,xd_des,f)
     robot.send_joint_command(tau)
+
+
+
+
+# plt.plot(tmp)
+# plt.show()
--- python/py_dcm_vrp_planner/planner.py
+++ python/py_dcm_vrp_planner/planner.py
@@ -64,13 +64,13 @@ class dcm_vrp_planner:
             B_l = self.t_min
             B_u = self.t_max
         else:
-            B_l = np.max([self.l_min/(self.v_des[0]), self.w_min/self.v_des[1], self.t_min])
-            B_u = np.max([self.l_max/(self.v_des[0]), self.w_max/self.v_des[1], self.t_max])
+            B_l = np.max([self.l_min/abs(self.v_des[0]), self.w_min/abs(self.v_des[1]), self.t_min])
+            B_u = np.max([self.l_max/abs(self.v_des[0]), self.w_max/abs(self.v_des[1]), self.t_max])
             
         t_nom = (B_l + B_u)/2.0
         l_nom = self.v_des[0] * t_nom
         w_nom = self.v_des[1] * t_nom
-                
+                        
         bx_nom = l_nom / (np.power(np.e, self.omega*t_nom) - 1)
         by_nom = np.power(-1, n) * (self.l_p/(1 + np.power(np.e, self.omega*t_nom))) - \
                         w_nom / (1 - np.power(np.e, self.omega*t_nom))
@@ -99,9 +99,20 @@ class dcm_vrp_planner:
     
         assert(np.shape(u) == (2,))
         assert(np.shape(psi_current) == (2,))
+
+        l_nom, w_nom, t_nom, bx_nom, by_nom = self.compute_nominal_step_values(n)
         
+        t_nom = np.power(np.e, self.omega*t_nom) ### take exp as T is considered as e^wt in qp
+        
+
+        ## note : add the weight values in the p and q array corectly
         P = np.identity(5) ## quadratic cost matrix
-        q = np.zeros(5) ## quadratic cost vector
+
+        q = np.array([-2*l_nom,
+                      -2*w_nom,
+                      -2*t_nom,
+                      -2*bx_nom,
+                      -2*by_nom ]) ## quadratic cost vector
     
         G = np.matrix([ [1, 0, 0, 0, 0],
                         [0, 1, 0, 0, 0],
@@ -114,20 +125,17 @@ class dcm_vrp_planner:
                         [0, 0, 0, -1, 0],
                         [0, 0, 0, 0, -1]])
     
-        l_nom, w_nom, t_nom, bx_nom, by_nom = self.compute_nominal_step_values(n)
         
-        t_nom = np.power(np.e, self.omega*t_nom) ### take exp as T is considered as e^wt in qp
-        
-        h = np.array([self.l_max - l_nom,
-                    self.w_max - w_nom,
-                    -1*self.l_min + l_nom,
-                    -1*self.w_min + w_nom,
-                    np.power(np.e, self.omega*self.t_max) - t_nom,
-                    -1*np.power(np.e, self.omega*self.t_min) + t_nom,
-                    self.bx_max - bx_nom,
-                    self.by_max_in - by_nom,
-                    -1*self.bx_min + bx_nom,
-                    -1*self.by_max_out + by_nom]) ## could be a problem if qp doesn't solve correctly
+        h = np.array([self.l_max,
+                      self.w_max,
+                     -1*self.l_min,
+                     -1*self.w_min,
+                     np.power(np.e, self.omega*self.t_max),
+                     -1*np.power(np.e, self.omega*self.t_min),
+                     self.bx_max,
+                     self.by_max_in,
+                     -1*self.bx_min,
+                     -1*self.by_max_out]) ## could be a problem if qp doesn't solve correctly
         
         tmp = [0.,0.]
         tmp[0] = (psi_current[0] - u[0])*np.power(np.e, -1*self.omega*t)
@@ -136,8 +144,8 @@ class dcm_vrp_planner:
         A = np.matrix([[1, 0, -1*tmp[0], 1, 0],
                         [0, 1, -1*tmp[1], 0, 1]])
         
-        b = np.array([-1*l_nom - bx_nom + tmp[0]*t_nom,
-                      -1*w_nom - by_nom + tmp[1]*t_nom])
+        b = np.array([u[0],
+                      u[1]])
     
         
         P = P.astype(float)
@@ -149,8 +157,68 @@ class dcm_vrp_planner:
         
         x_opt = quadprog_solve_qp(P,q, G, h, A, b)
         t_end = np.log(x_opt[2] + t_nom)/self.omega
-        # print(x_opt)
-        return (x_opt[0] + u[0] + l_nom, x_opt[1] + u[1] + w_nom, t_end)
+
+
+        return (x_opt[0], x_opt[1], t_end)
+
+
+    def generate_foot_trajectory(self, u_t_end, t_end, t, x_foot, z_max, z_ground, ctrl_timestep = 0.001):
+        '''
+            This function generates a linear trajectory from the current foot location
+            to the desired step location and returns the desired location of the foot 
+            at the next step.
+            
+            Input :
+                u_t_end : desried step location
+                t_end : time duration of the step
+                t : current timestep
+                x_foot : current location of foot
+                z_max : maximum height the foot should reach(will reach at middle of the step time)
+                z_ground : the location of the ground
+                ctrl_timestep : the timestep at which value is recomputed
+        '''
+        
+        x_foot_des = np.zeros(3)
+        xd_foot_des = np.zeros(3)
+        
+        
+        if x_foot[2] < z_ground:
+            x_foot[2] = z_ground
+        
+        x_foot_des[0] = np.linspace(x_foot[0], u_t_end[0], num= (t_end - t)/ctrl_timestep)[1]
+        x_foot_des[1] = np.linspace(x_foot[1], u_t_end[1], num= (t_end - t)/ctrl_timestep)[1]
+        
+        if t < t_end/2.0 :
+            lin_space = np.linspace(x_foot[2], z_max, num= ((t_end/2.0) - t)/(ctrl_timestep))
+            if len(lin_space) > 1 :
+                x_foot_des[2] = lin_space[1]
+            else:
+                x_foot_des[2] = x_foot[2]
+            xd_foot_des[0] = (t - t_end/2.0)*(t - t_end/2.0)*self.v_des[0]
+            xd_foot_des[1] = (t - t_end/2.0)*(t - t_end/2.0)*self.v_des[1]
+            xd_foot_des[2] = (t - t_end/2.0)*(t - t_end/2.0)*1.2     
+        elif t == t_end/2.0 :
+            x_foot_des[2] = z_max
+            
+        else:
+            lin_space = np.linspace(x_foot[2], z_ground, num= (t_end - t)/(ctrl_timestep))
+            if len(lin_space) > 1 :
+                x_foot_des[2] = lin_space[1]
+            else:
+                x_foot_des[2] = z_ground
+            
+            xd_foot_des[0] = (t - t_end)*(t - t_end)*self.v_des[0]
+            xd_foot_des[1] = (t - t_end)*(t - t_end)*self.v_des[1]
+            xd_foot_des[2] = (t - t_end)*(t - t_end)*1.2     
+
+        if x_foot_des[2] < z_ground:
+            print(t, t_end)
+            print(x_foot[2])
+            assert False
+
+                                               
+        return x_foot_des, xd_foot_des
+    
     
     
     
